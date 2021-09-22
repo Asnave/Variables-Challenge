2066acad97f562a6b34ab90bbb7a94a9228f24e4
Abigail Evans <90458573+Asnave@users.noreply.github.com>
2021-09-22 09:06:48 -0300

    Story Update

--- Variables Challenge/Program.cs
+++ Variables Challenge/Program.cs
@@ -14,6 +14,7 @@ namespace Variables_Challenge
             gameTitle = "The Legend Of Thyme";
             Console.WriteLine(gameTitle);
 
+            Console.ReadKey(true);
 
             Console.WriteLine("");
 
@@ -21,7 +22,7 @@ namespace Variables_Challenge
             // Game Factors ------
 
             int cookingPoints;
-            cookingPoints = 100;
+            cookingPoints = 0;
 
             int damage;
             damage = 0;
@@ -34,6 +35,10 @@ namespace Variables_Challenge
 
             int cookingExperience;
             cookingExperience = 0;
+
+
+
+
 
 
 
@@ -58,25 +63,21 @@ namespace Variables_Challenge
             float shield;
             shield = 100;
             shield = shield / shieldDamage;
-            Console.WriteLine("Shield Percatage: " + shield);
+
 
             // Lives -------
 
             int lives;
             lives = 3;
 
-            if (health < 10)
-            {
-                lives = lives - 1;
 
-            }
 
             Console.WriteLine("Lives:" + lives);
 
             // Multiplier / experience points  --------
 
 
-            Console.WriteLine("XP Multiplier: " + multiplier);
+
 
             float experiencePoints;
             experiencePoints = 0;
@@ -86,6 +87,7 @@ namespace Variables_Challenge
 
             Console.WriteLine("ExperiencePoints: " + experiencePoints);
             Console.WriteLine("---------------------");
+            Console.WriteLine("");
 
             Console.WriteLine("You wake up in a kitchen, a note on the counter states:" +
                 " for you to pass you must cook your first dish...");
@@ -93,22 +95,23 @@ namespace Variables_Challenge
             Console.ReadKey(true);
             Console.WriteLine("");
 
+
             // Game Factors ------
 
-         
-            cookingPoints = 100;
 
-            
+            cookingPoints = 0;
+
+
             damage = -5;
 
-            
-            shieldDamage = 2;
 
-            
-            multiplier = 3.0f / 2.0f ;
+            shieldDamage = 0;
 
-            
-            cookingExperience = 10;
+
+            multiplier = 0;
+
+
+            cookingExperience = +10;
 
 
 
@@ -123,27 +126,247 @@ namespace Variables_Challenge
 
             // Health -----
 
-            
+
             health = 10;
             health = health + damage;
             Console.WriteLine("Health: " + health);
 
             // Shield -------
 
-            
+
             shield = 100;
             shield = shield / shieldDamage;
-            Console.WriteLine("Shield Percatage: "+ shield);
+
 
             // Lives -------
 
-            
+
+
+
+            Console.WriteLine("Lives:" + lives);
+
+            // Multiplier / experience points  --------
+
+
+
+
+
+            experiencePoints = 0f;
+
+            experiencePoints = experiencePoints + cookingExperience;
+
+
+            Console.WriteLine("ExperiencePoints: " + experiencePoints);
+            Console.WriteLine("---------------------");
+            Console.WriteLine("");
+
+            Console.WriteLine("You begin with cutting the vegetables laid out in the rustic looking pantry,");
+            Console.WriteLine(" unfortunitly for you,");
+            Console.WriteLine("you notice the lack of experience points you have in cooking.");
+            Console.WriteLine("daydreaming, you mistaken your finger to be one of the carrots, cutting the tip clean off.");
+            Console.WriteLine("Taking -5 Damage");
+            Console.WriteLine("Gaining +5 experience points knowing you wont make that mistake again....");
+            Console.WriteLine("");
+            Console.ReadKey(true);
+
+            // Game Factors ------
+
+
+            cookingPoints = +5;
+
+
+            damage = -5;
+
+
+            shieldDamage = 2f;
+
+
+            multiplier = 0f;
+
+
+            cookingExperience = +10;
+
+
+
+
+            //Score ---------
+
+            Console.WriteLine("---------HUD---------");
+            score = 0;
+
+            score = score + cookingPoints;
+            Console.WriteLine("Score: " + score);
+
+            // Health -----
+
+
+            health = 10;
+            health = health + damage;
+            Console.WriteLine("Health: " + health);
+
+            // Shield -------
+
+
+            shield = 100;
+            shield = shield / shieldDamage;
+            Console.WriteLine("FryingPan Percatage: " + shield);
+
+            // Lives -------
+
+
+
+
+            Console.WriteLine("Lives:" + lives);
+
+            // Multiplier / experience points  --------
+
+
+
+
+
+            experiencePoints = 0;
+
+            experiencePoints = experiencePoints + cookingExperience;
+
+
+            Console.WriteLine("ExperiencePoints: " + experiencePoints);
+            Console.WriteLine("---------------------");
+            Console.WriteLine("");
+
+            Console.WriteLine("Looking around you found a frying pan,");
+            Console.WriteLine("Weilding the frying pan allows you to saute the now cut vegtables");
+            Console.WriteLine("not really kowing what your doing, you start burning everything, including the pan.");
+            Console.WriteLine("Fortunitly it shielded the hot oils from your eyes with the cover");
+            Console.WriteLine("Your Frying Pan taking half its utillity Percentage.");
+            Console.WriteLine("Gaining +5 Cooking points to your Score for cleaverness.");
+            Console.WriteLine("");
+            Console.ReadKey(true);
+
+            // Game Factors ------
+
+
+            cookingPoints = 25;
+
+
+            damage = -5;
+
+
+            shieldDamage = 2;
+
+
+            multiplier = 3.0f / 2.0f;
+
+
+            cookingExperience = 15;
+
+
+
+
+            //Score ---------
+
+            Console.WriteLine("---------HUD---------");
+            score = 0;
+
+            score = score + cookingPoints;
+            Console.WriteLine("Score: " + score);
+
+            // Health -----
+
+
+            health = 10;
+            health = health + damage;
+            Console.WriteLine("Health: " + health);
+
+            // Shield -------
+
+
+            shield = 100;
+            shield = shield / shieldDamage;
+            Console.WriteLine("Shield Percatage: " + shield);
+
+            // Lives -------
+
+
+
+
+            Console.WriteLine("Lives:" + lives);
+
+            // Multiplier / experience points  --------
+
+
+            Console.WriteLine("XP Multiplier: " + multiplier);
+
+
+            experiencePoints = 0;
+
+            experiencePoints = experiencePoints + cookingExperience;
+            experiencePoints = experiencePoints * multiplier;
+
+            Console.WriteLine("ExperiencePoints: " + experiencePoints);
+            Console.WriteLine("---------------------");
+            Console.WriteLine("");
+
+            Console.WriteLine("Now to the protine!");
+            Console.WriteLine("Taking the obvious choice of chicken, needing to butcher it first");
+            Console.WriteLine("You take the best part of the meat, spitting it over a fire till you get medieval style rotisserie.");
+            Console.WriteLine("picking through the scraps you find one of the legs of the chicken, finding out its a charm of sorts");
+            Console.WriteLine("Granting you with a small XP Multiplier!");
+            Console.WriteLine("You gain +20 cooking points for the lack of chaos");
+            Console.WriteLine("You gain +5 Experience points");
+            Console.WriteLine("");
+            Console.ReadKey(true);
+
+            // Game Factors ------
+
+
+            cookingPoints = 100;
+
+
+            damage = -5;
+
+
+            shieldDamage = 2;
+
+
+            multiplier = 3.0f / 2.0f;
+
+
+            cookingExperience = 30;
+
+
+
+
+            //Score ---------
+
+            Console.WriteLine("---------HUD---------");
+            score = 0;
+
+            score = score + cookingPoints;
+            Console.WriteLine("Score: " + score);
+
+            // Health -----
+
+
+            health = 10;
+            health = health + damage;
+            Console.WriteLine("Health: " + health);
+
+            // Shield -------
+
+
+            shield = 100;
+            shield = shield / shieldDamage;
+            Console.WriteLine("Shield Percatage: " + shield);
+
+            // Lives -------
+
+
             lives = 3;
 
-            if (health < 10) 
+            if (health <= 0)
             {
                 lives = lives - 1;
-                
+
             }
 
             Console.WriteLine("Lives:" + lives);
@@ -153,26 +376,39 @@ namespace Variables_Challenge
 
             Console.WriteLine("XP Multiplier: " + multiplier);
 
-            
+
             experiencePoints = 0;
 
             experiencePoints = experiencePoints + cookingExperience;
             experiencePoints = experiencePoints * multiplier;
 
             Console.WriteLine("ExperiencePoints: " + experiencePoints);
+
+            // Level------------
+
+            int Level;
+            Level = 1;
+
+            if (experiencePoints >= 40)
+            {
+                Level = Level + 1;
+            }
+
+            Console.WriteLine("Level: " + Level);
+
+
             Console.WriteLine("---------------------");
 
             Console.WriteLine(" ");
-            Console.WriteLine("As you finsished your first dish you notice : ");
+            Console.WriteLine("As you finsished your dish you notice : ");
             Console.WriteLine("");
-            Console.WriteLine("You gained 100 cooking points");
-            Console.WriteLine("you took -5 damange");
-            Console.WriteLine("your Frying Pan shield took half its damage");
-            Console.WriteLine("Your Experience Points grew to + 15 with your XP Multiplier engaged");
+            Console.WriteLine("A door starts to open allowing you to leave, ");
+            Console.WriteLine("With that you gain +100 cooking points");
+            Console.WriteLine("Aswell as +15 XP");
+            Console.WriteLine("");
 
 
             Console.ReadKey(true);
-
         }
     }
 }
